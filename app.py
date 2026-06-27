34a0e578cb5471f9e1d83d20f171553dc23aefa6
24f2005132 <24f2005132@ds.study.iitm.ac.in>
2026-06-27 00:59:55 +0000

    Player details scraping complete!

--- app.py
+++ app.py
@@ -9,28 +9,56 @@ import time
 print("Setting up the invisible browser...")
 
 options = webdriver.ChromeOptions()
-options.add_argument('--no-sandbox')             # Bypass OS security model
-options.add_argument('--disable-dev-shm-usage')  # Prevent memory crashes
-
-# Step 1: Initialize the browser (Selenium handles the driver automatically now!)
+options.add_argument('--headless=new')
+options.add_argument('--no-sandbox')           
+options.add_argument('--disable-dev-shm-usage') 
 driver = webdriver.Chrome(options=options)
-
-# Step 2: Navigate to the dynamic website
+urls=['https://www.transfermarkt.com/spieler-statistik/wertvollstemannschaften/marktwertetop']
+first=False
+row_data=[]
+count=0
 print("Opening the website...")
-driver.get("https://www.transfermarkt.com/liverpool-fc/kader/verein/31/saison_id/2025/plus/1")
+for url in urls:
+    count=count+1
+    driver.get(url)
+    print(f"Waiting 1 second for JavaScript to load webpage {count}...")
 
-# Step 3: Wait for the JavaScript to "build" the page
-print("Waiting 3 seconds for JavaScript to load the data...")
-time.sleep(3) 
 
-# Step 4: Locate the data we want to scrape
-print("Extracting data...\n")
-quotes = driver.find_elements(By.CSS_SELECTOR, "td")
+    print("Extracting data...\n")
+    if first==False:
+        rows=driver.find_elements(By.CSS_SELECTOR, "table.items > tbody > tr")
+        for row in rows:
+            cell = row.find_elements(By.TAG_NAME,"td")[1]
+            link = cell.find_element(By.TAG_NAME, "a").get_attribute("href")
+            print(link)
+            urls.append(link)
+        first=True
+        continue
+    rows = driver.find_elements(By.CSS_SELECTOR, "table.items > tbody > tr")
+    links = driver.find_elements(By.CSS_SELECTOR, "thead th a")
 
-# Step 5: Clean and display the data
-for quote in quotes:
-    print(quote.text)
+    columns = [link.get_attribute("textContent").strip() for link in links]
 
-# Step 6: Close the browser to free up memory
+    for row in rows:
+        cells = row.find_elements(By.TAG_NAME, "td")
+        cleaned_row = []
+        second_cell=row.find_elements(By.TAG_NAME,"td")[1]
+        player_link = second_cell.find_elements(By.TAG_NAME, "a")
+        for cell in cells:
+            text = " ".join(cell.get_attribute("textContent").split())
+            if text: 
+                cleaned_row.append(text)
+        row_data.append(cleaned_row)
+    
+for row in row_data:
+    del row[1]
+columns[0] = 'Shirt No.'
+columns.insert(2,'Position')
+'''columns.append("Appearances")
+columns.append("Goals")
+columns.append("Assists")'''
+df=pd.DataFrame(row_data,columns=columns)
+print(df)
 driver.quit()
 print("Scraping complete!")
+
